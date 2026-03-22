0ee060f01286bdd2dcc2418d1346993d433987d2
GGSkyOne <97788417+GGSkyOne@users.noreply.github.com>
2026-03-22 09:07:07 +0300

    fix: item slowdown abilities

--- src/main/java/io/github/apace100/originsclasses/mixin/ClientPlayerEntityMixin.java
+++ src/main/java/io/github/apace100/originsclasses/mixin/ClientPlayerEntityMixin.java
@@ -9,10 +9,12 @@ import net.minecraft.item.BowItem;
 import net.minecraft.item.ItemStack;
 import net.minecraft.item.ShieldItem;
 import net.minecraft.util.Hand;
+import net.minecraft.util.math.Vec2f;
 import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.Shadow;
-import org.spongepowered.asm.mixin.injection.Constant;
-import org.spongepowered.asm.mixin.injection.ModifyConstant;
+import org.spongepowered.asm.mixin.injection.At;
+import org.spongepowered.asm.mixin.injection.Inject;
+import org.spongepowered.asm.mixin.injection.callback.CallbackInfoReturnable;
 
 @Mixin(ClientPlayerEntity.class)
 public abstract class ClientPlayerEntityMixin extends AbstractClientPlayerEntity {
@@ -22,23 +24,20 @@ public abstract class ClientPlayerEntityMixin extends AbstractClientPlayerEntity
 
     @Shadow public abstract Hand getActiveHand();
 
-    @ModifyConstant(
-        method = "tickMovement",
-        constant = @Constant(
-            floatValue = 0.2F
-        )
+    @Inject(
+        method = "applyMovementSpeedFactors",
+        at = @At("RETURN"),
+        cancellable = true
     )
-    private float modifyItemUseSlowdown(float originalSlowdown) {
+    private void modifyItemUseSlowdown(Vec2f input, CallbackInfoReturnable<Vec2f> cir) {
         ItemStack stackInUse = this.getStackInHand(this.getActiveHand());
 
         if (stackInUse.getItem() instanceof ShieldItem && ClassesPowerTypes.LESS_SHIELD_SLOWDOWN.isActive(this)) {
-            return 0.6F;
+            Vec2f r = cir.getReturnValue();
+            cir.setReturnValue(new Vec2f(r.x * 3.0f, r.y * 3.0f));
+        } else if (stackInUse.getItem() instanceof BowItem && ClassesPowerTypes.LESS_BOW_SLOWDOWN.isActive(this)) {
+            Vec2f r = cir.getReturnValue();
+            cir.setReturnValue(new Vec2f(r.x * 3.0f, r.y * 3.0f));
         }
-
-        if (stackInUse.getItem() instanceof BowItem && ClassesPowerTypes.LESS_BOW_SLOWDOWN.isActive(this)) {
-            return 0.6F;
-        }
-
-        return originalSlowdown;
     }
 }
