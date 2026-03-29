ccba2bfa9b1fc31f7f4007beba437267a2a5f81b
GGSkyOne <97788417+GGSkyOne@users.noreply.github.com>
2026-03-29 20:57:30 +0300

    fix: don't supress block break particles in stealth

--- src/main/java/io/github/apace100/originsclasses/mixin/ClientLevelMixin.java
+++ src/main/java/io/github/apace100/originsclasses/mixin/ClientLevelMixin.java
@@ -4,6 +4,8 @@ import io.github.apace100.originsclasses.effect.ClassesEffects;
 import net.minecraft.client.multiplayer.ClientLevel;
 import net.minecraft.world.entity.Entity;
 import net.minecraft.world.entity.LivingEntity;
+import net.minecraft.world.level.block.Block;
+import net.minecraft.world.level.block.state.BlockState;
 import net.minecraft.core.BlockPos;
 import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.injection.At;
@@ -18,8 +20,15 @@ public class ClientLevelMixin {
         cancellable = true
     )
     private void cancelEffectsDuringStealth(Entity entity, int eventId, BlockPos pos, int data, CallbackInfo ci) {
-        if (entity instanceof LivingEntity livingEntity && livingEntity.hasEffect(ClassesEffects.STEALTH)) {
+        if (!(entity instanceof LivingEntity livingEntity) || !livingEntity.hasEffect(ClassesEffects.STEALTH)) return;
+
+        if (eventId == 2001) {
             ci.cancel();
+            BlockState state = Block.stateById(data);
+
+            if (!state.isAir()) {
+                ((ClientLevel) (Object) this).addDestroyBlockEffect(pos, state);
+            }
         }
     }
 }
